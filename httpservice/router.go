4bb53596df37cefd209fa8d0221e8263b12f4122
Nicolas Maupu <nmaupu@gmail.com>
2021-02-07 14:37:35 +0100

    Fix handler for sunrise / sunset

--- httpservice/router.go
+++ httpservice/router.go
@@ -62,7 +62,7 @@ func (s *httpService) Start() error {
 	router.GET("/health", controllers.HealthHandler)
 	router.GET("/google-validate", controllers.GoogleWebTokenHandler)
 	router.GET("/coords", controllers.CoordsHandler)
-	router.GET("/sun", controllers.CoordsHandler)
+	router.GET("/sun", controllers.SunriseSunsetHandler)
 
 	s.server = &http.Server{
 		Addr:    fmt.Sprintf("%s:%d", s.BindAddr, s.Port),
